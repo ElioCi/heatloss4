b9f2304cd65f57336b576e35d79188649fe571de
ElioCi <107808771+ElioCi@users.noreply.github.com>
2025-01-15 11:52:10 +0100

    Update 3_📐Units_of_measurement.py

--- pages/3_📐Units_of_measurement.py
+++ pages/3_📐Units_of_measurement.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('📐 Units of measurement')
 #leggi dati di input da DatiGenerali.csv
-with open('files/Units.csv') as file_units:
+with open('files/units.csv') as file_units:
     dfunits = pd.read_csv(file_units, delimiter= ',', encoding="utf-8")   # lettura file e creazione
     #dfunits.drop(dfunits.columns[dfunits.columns.str.contains('unnamed', case= False)], axis=1, inplace= True)
     dfunits.columns = dfunits.columns.str.replace(r"[^a-zA-Z0-9_ ]", "", regex=True)  # Rimuove caratteri non alfanumerici

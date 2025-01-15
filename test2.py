fadae255ac46c00e5bea0113d6f2480798b5c2f7
ElioCi <107808771+ElioCi@users.noreply.github.com>
2025-01-15 13:49:58 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,18 +9,34 @@ from datetime import datetime
 
 from calcoli import Irraggiamento, Convezione, Conduzione, Dispersione
 from altair_saver import save
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+#from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
 from PIL import Image
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
 
 
 
 # Salva il grafico come immagine
 def save_chart_as_image(chart, filename='files/grafico.png'):
     # Configurazione del driver Selenium per il rendering
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Esegui in modalità headless
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')  # Esegui in modalità headless
+    #driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    options = Options()
+    options.add_argument("--headless")  # Modalità headless
+    options.add_argument("--no-sandbox")  # Necessario per ambienti cloud
+    options.add_argument("--disable-dev-shm-usage")  # Risolve problemi di memoria
+    options.add_argument("--disable-gpu")  # Migliora compatibilità
+
+    service = Service("/usr/bin/chromedriver")  # Percorso del driver
+    driver = webdriver.Chrome(service=service, options=options)
     
     # Usa Streamlit per renderizzare il grafico in un file HTML temporaneo
     chart.save('files/grafico.html')
